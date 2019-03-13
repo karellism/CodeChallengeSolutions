48145becfd60377d70d8218d73b34e8169767832
Karel Vanhelden <karellism@users.noreply.github.com>
2019-03-13 21:43:41 +0100

    Create solution.cpp

---
+++ LeetCode/Problem69/C++/solution.cpp
@@ -0,0 +1,9 @@
+// Solution in C++ to Problem 69 on LeetCode - Sqrt(x)
+// By Karel Vanhelden
+class Solution {
+public:
+    int mySqrt(int x) {
+        int ans = pow(x,0.5);
+        return ans;
+    }
+};
